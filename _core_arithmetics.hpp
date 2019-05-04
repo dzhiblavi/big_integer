cedf19c0eae531b93a04bcb78bd47ed318a39218
dzhiblavi <dzhiblavi@gmail.com>
2019-05-04 16:32:34 +0300

    code

--- _core_arithmetics.hpp
+++ _core_arithmetics.hpp
@@ -18,7 +18,7 @@
 
 namespace _core {
     extern "C" {
-//        uint64_t _asm_add(uint64_t *, uint64_t const *, size_t);
+        uint64_t _asm_add(uint64_t *, uint64_t const *, size_t);
         uint64_t _asm_sub(uint64_t *, uint64_t const *, size_t);
         uint64_t _asm_mul(uint64_t *, uint64_t const *, uint64_t const *, size_t, size_t);
         uint64_t _asm_short_add(uint64_t *, uint64_t, size_t);
